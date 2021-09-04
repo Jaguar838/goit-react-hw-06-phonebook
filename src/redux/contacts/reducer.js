154035c21a2061ac3c5c608f90a8abf899bf4d8e
Oleg Yamgurov <75248620+Jaguar838@users.noreply.github.com>
2021-09-04 11:14:05 +0300

    Update reducer.js

--- src/redux/contacts/reducer.js
+++ src/redux/contacts/reducer.js
@@ -8,16 +8,6 @@ import {
     changeFilter,
 } from './actions';
 
-// Начальное состояние
-// const initialState = {
-//     contacts: [
-//         { id: 'id-1', name: 'Rosie Simpson', phone: '459-12-56' },
-//         { id: 'id-2', name: 'Hermione Kline', phone: '443-89-12' },
-//         { id: 'id-3', name: 'Eden Clements', phone: '645-17-79' },
-//         { id: 'id-4', name: 'Annie Copeland', phone: '227-91-26' },
-//     ],
-// };
-
 // Прописывае в reducer логику взаимодействия с экшенами, где обработка каждого
 //    экшена это свойство обьекта
 // ключом являеться имя экшена(в[]) а значением результат вызова анонимной стрелочной ф-и.
